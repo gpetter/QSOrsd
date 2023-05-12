d2b5e0d3c9254ec88b39599b99be2b01b2fbeb04
gpetter <graysonpetter@gmail.com>
2023-05-12 14:29:02 -0400

    Ability for gaussian priors on parameters

--- abacus_mcmc.py
+++ abacus_mcmc.py
@@ -1,36 +1,36 @@
 import numpy as np
-import yaml
-from abacusnbody.hod.abacus_hod import AbacusHOD
 import emcee
 import mcmc
 
-
-def ln_prior(theta, param_names):
-
-    param_bounds = {'logM_cut': (11.7, 12.6), 'logM1': (13.3, 14.5), 'alpha_c': (0., 3.), 's': (-1., 1.),
+param_bounds = {'logM_cut': (11.7, 12.6), 'logM1': (13.3, 14.5), 'alpha_c': (0., 1.5), 's': (-1., 1.),
                     's_v': (-1., 1.), 'Bcent': (-1., 1.), 'Bsat': (-1., 1.)}
+
+default_param_priors = {'logM_cut': (12.2, 0.5), 'alpha_c': (0.5, 0.3), 's': (0., 0.3),
+                    's_v': (0., 0.3), 'Bcent': (0., 0.3), 'Bsat': (0., 0.3)}
+
+def ln_prior(theta, param_names, gausspriors):
     priorsum = 0
     for j in range(len(theta)):
         paramval = theta[j]
-        if (paramval >= param_bounds[param_names[j]][0]) and (paramval <= param_bounds[param_names[j]][1]):
-            priorsum += 0.
-        else:
-            priorsum += -np.inf
+        if (paramval <= param_bounds[param_names[j]][0]) or (paramval >= param_bounds[param_names[j]][1]):
+            priorsum -= np.inf
+            return priorsum
+        mu_j, sig_j = gausspriors[j]
+        priorsum -= 0.5 * ((paramval - mu_j) / sig_j) ** 2
     return priorsum
 
 
-def ln_prob_abacushod(theta, param_names, cf2d_observed, ball, force_qso_satellites, nthread=24):
+def ln_prob_abacushod(theta, param_names, cf2d_observed, ball, force_qso_satellites, gausspriors, nthread=24):
     rp_bins = cf2d_observed['rp_bins']
     pimax = int(cf2d_observed['pimax'])
     pi_bin_size = int(cf2d_observed['dpi'])
     cf_obs = np.array(cf2d_observed['xi_rp_pi']).ravel()
     cf_obs_err = np.array(cf2d_observed['xi_rp_pi_err']).ravel()
 
-    ball.tracers['QSO']['sigma'] = 1e-4
-
-    prior = ln_prior(theta, param_names)
-    if np.isfinite(prior):
-
+    prior = ln_prior(theta, param_names, gausspriors)
+    if not np.isfinite(prior):
+        return -np.inf
+    else:
         for j in range(len(theta)):
             ball.tracers['QSO'][param_names[j]] = theta[j]
         mock_dict = ball.run_hod(ball.tracers, want_rsd=True, write_to_disk=False, Nthread=nthread, verbose=False,
@@ -39,36 +39,31 @@ def ln_prob_abacushod(theta, param_names, cf2d_observed, ball, force_qso_satelli
                                      pimax=pimax, pi_bin_size=pi_bin_size)['QSO_QSO'].ravel()
         residual = cf_obs - xirppi
         likely = mcmc.ln_likelihood(residual=residual, yerr=cf_obs_err)
+        return likely + prior
 
-    else:
-        likely = -np.inf
-
-    return likely + prior
 
 def sample_abacus_hod(nwalkers, niter, cf2d, param_names, ball, initial_params=None, pool=None,
-                      force_qso_satellites=False, nthread=1):
+                      force_qso_satellites=False, gausspriors=None, nthread=1):
+
+    if gausspriors is None:
+        gausspriors = [[default_param_priors[name][0], default_param_priors[name][1]] for name in param_names]
+        init_scatter = 2e-1
+    else:
+        init_scatter = np.array(gausspriors)[:, 1] / 2.
 
     ndim = len(param_names)
-
-    #newBall = AbacusHOD(sim_params, HOD_params, clustering_params)
-    #mock_dict = newBall.run_hod(newBall.tracers, want_rsd, write_to_disk, Nthread=nthread)
-
-
     sampler = emcee.EnsembleSampler(nwalkers, ndim,
                                     ln_prob_abacushod,
-                                    args=[param_names, cf2d, ball, force_qso_satellites, nthread], pool=pool)
-
+                                    args=[param_names, cf2d, ball, force_qso_satellites, gausspriors, nthread],
+                                    pool=pool)
 
     # start walkers near least squares fit position with random gaussian offsets
-    pos = np.array(initial_params) + 2e-1 * np.random.normal(size=(sampler.nwalkers, sampler.ndim))
-
+    pos = np.array(initial_params) + np.random.normal(size=(sampler.nwalkers, sampler.ndim)) * init_scatter
     sampler.run_mcmc(pos, niter, progress=True)
 
 
-    flatchain = sampler.get_chain(discard=10, flat=True)
+    flatchain = sampler.get_chain(discard=int(niter/10), flat=True)
     #blobs = sampler.get_blobs(discard=10, flat=True)
-
-
 
     centervals, lowerrs, higherrs = [], [], []
     for i in range(ndim):
@@ -77,7 +72,5 @@ def sample_abacus_hod(nwalkers, niter, cf2d, param_names, ball, initial_params=N
         centervals.append(post[1])
         lowerrs.append(q[0])
         higherrs.append(q[1])
-
-
 
     return flatchain, centervals, lowerrs, higherrs
